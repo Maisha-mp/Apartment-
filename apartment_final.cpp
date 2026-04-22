00a2df5e032ef1a2654a082087b321de7a7b3557
Al Kabir <kabir23105101151@diu.edu.bd>
2026-04-22 22:10:33 +0600

    Al Kabir: Added Bresenham line drawing algorithm

--- apartment_final.cpp
+++ apartment_final.cpp
@@ -103,6 +103,25 @@ void drawCircleMidpoint(int xc, int yc, int r)
     glEnd();
 }
 
+//Kabir: Bresenham Line
+
+void drawLineBresenham(int x1, int y1, int x2, int y2) {
+    int dx  = std::abs(x2 - x1);
+    int dy  = std::abs(y2 - y1);
+    int sx  = (x1 < x2) ? 1 : -1;
+    int sy  = (y1 < y2) ? 1 : -1;
+    int err = dx - dy;
+    glBegin(GL_POINTS);
+    while (true) {
+        plotPoint(x1, y1);
+        if (x1 == x2 && y1 == y2) break;
+        int e2 = 2 * err;
+        if (e2 > -dy) { err -= dy; x1 += sx; }
+        if (e2 <  dx) { err += dx; y1 += sy; }
+    }
+    glEnd();
+}
+
 // Maisha: basic shapes
 
 void filledRect(float x1, float y1, float x2, float y2, float r, float g, float b)
